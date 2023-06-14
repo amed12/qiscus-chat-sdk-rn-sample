0e8c79e1d0920f30eb3594f7423fa90d6de5ff70
Achmad Fathullah <amedixro@gmail.com>
2023-06-14 10:45:07 +0700

    feat(image-picker): change to sinarmas lib
    adapt to old library image picker

--- android/app/src/main/java/com/qiscus/chat/sample/MainApplication.java
+++ android/app/src/main/java/com/qiscus/chat/sample/MainApplication.java
@@ -10,7 +10,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.imagepicker.ImagePickerPackage;
+// import com.imagepicker.ImagePickerPackage;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
@@ -29,7 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-          packages.add(new ImagePickerPackage());
+//           packages.add(new ImagePickerPackage());
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           return packages;
