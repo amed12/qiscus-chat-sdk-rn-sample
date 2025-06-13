c85830bc504b56b090601277f533dea0b0adc016
Achmad Fathullah <fathullah@qiscus.co>
2025-06-13 22:20:35 +0700

    refactor: rename app package from ReactNativeSampleQiscus to QiscusChatSDKRNSample and update Android configurations

--- android/app/src/main/java/com/qiscus/chat/sample/newarchitecture/MainApplicationReactNativeHost.java
+++ android/app/src/main/java/com/qiscus/chat/sample/newarchitecture/MainApplicationReactNativeHost.java
@@ -1,4 +1,4 @@
-package com.reactnativesampleqiscus.newarchitecture;
+package com.qiscus.chat.sample.newarchitecture;
 
 import android.app.Application;
 import androidx.annotation.NonNull;
@@ -19,9 +19,9 @@ import com.facebook.react.fabric.CoreComponentsRegistry;
 import com.facebook.react.fabric.FabricJSIModuleProvider;
 import com.facebook.react.fabric.ReactNativeConfig;
 import com.facebook.react.uimanager.ViewManagerRegistry;
-import com.reactnativesampleqiscus.BuildConfig;
-import com.reactnativesampleqiscus.newarchitecture.components.MainComponentsRegistry;
-import com.reactnativesampleqiscus.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
+import com.qiscus.chat.sample.BuildConfig;
+import com.qiscus.chat.sample.newarchitecture.components.MainComponentsRegistry;
+import com.qiscus.chat.sample.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
 import java.util.ArrayList;
 import java.util.List;
 
--- android/app/src/main/java/com/qiscus/chat/sample/newarchitecture/components/MainComponentsRegistry.java
+++ android/app/src/main/java/com/qiscus/chat/sample/newarchitecture/components/MainComponentsRegistry.java
@@ -1,4 +1,4 @@
-package com.reactnativesampleqiscus.newarchitecture.components;
+package com.qiscus.chat.sample.newarchitecture.components;
 
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
